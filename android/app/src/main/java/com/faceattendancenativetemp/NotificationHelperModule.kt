e00592bdb6866ef7b3078e395d3a044f4fdcac80
developerMark17 <webdevairaz@gmail.com>
2026-06-15 01:08:01 +0530

    feat: add NotificationHelperModule to manage background services, sync tasks, and notification permissions

--- android/app/src/main/java/com/faceattendancenativetemp/NotificationHelperModule.kt
+++ android/app/src/main/java/com/faceattendancenativetemp/NotificationHelperModule.kt
@@ -3,6 +3,7 @@ package com.faceattendancenativetemp
 import android.content.Context
 import android.content.Intent
 import android.content.ComponentName
+import android.service.notification.NotificationListenerService
 import android.provider.Settings
 import android.util.Log
 import androidx.work.ExistingWorkPolicy
@@ -112,7 +113,7 @@ class NotificationHelperModule(reactContext: ReactApplicationContext) : ReactCon
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             try {
                 val componentName = ComponentName(reactApplicationContext, MyNotificationListenerService::class.java)
-                MyNotificationListenerService.requestRebind(componentName)
+                NotificationListenerService.requestRebind(componentName)
                 Log.d("NotificationHelper", "Requested NotificationListenerService rebind successfully.")
             } catch (e: Exception) {
                 Log.e("NotificationHelper", "Failed to request rebind", e)
